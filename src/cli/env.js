a352c711923426fc88d0464d02b1880fe72cbe05
Aleksei Golubkov <nuriver@gmail.com>
2024-09-25 20:49:49 +0200

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = process.env;
+  let output = "";
+
+  const targetVars = Object.entries(variables).filter(([key]) =>
+    key.startsWith("RSS_")
+  );
+
+  targetVars.forEach(([key, value], index) => {
+    if (index < targetVars.length - 1) {
+      output += `${key} = ${value}; `;
+    } else {
+      output += `${key} = ${value}`;
+    }
+  });
+  console.log(output);
 };
 
 parseEnv();
