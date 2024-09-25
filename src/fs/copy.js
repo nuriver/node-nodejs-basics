0a50e468aa203a2d7ba7af7f9ecc33fc2ec50b1e
Aleksei Golubkov <nuriver@gmail.com>
2024-09-25 16:53:19 +0200

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access("../fs/files");
+
+    try {
+      await fs.access("../fs/files_copy");
+      throw new Error("FS operation failed");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        await fs.cp("../fs/files", "../fs/files_copy", { recursive: true });
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
