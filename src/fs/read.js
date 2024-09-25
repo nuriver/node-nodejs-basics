cfc16505cbfd22291c585d9e9205d8c51eafc3f5
Aleksei Golubkov <nuriver@gmail.com>
2024-09-25 19:50:37 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs/promises";
+
+const isExist = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, "files", "fileToRead.txt");
+  
+  const fileToReadExist = await isExist(fileToReadPath);
+
+  if (fileToReadExist) {
+    const data = await fs.readFile(fileToReadPath, "utf8");
+    console.log(data);
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
