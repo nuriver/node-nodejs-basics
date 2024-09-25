c96e73d9efa65d258995fe2143d8e62220eaf6cd
Aleksei Golubkov <nuriver@gmail.com>
2024-09-25 21:17:16 +0200

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,26 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+  let output = '';
+
+  for (let i = 0; i < args.length; i += 1) {
+    if (args[i].startsWith('--')) {
+      const key = args[i].substring(2);
+      const value = args[i + 1];
+      parsedArgs[key] = value;
+      i += 1;
+    }
+  }
+  
+  const argsArray = Object.entries(parsedArgs);
+  argsArray.forEach(([key, value], index) => {
+    if (index < argsArray.length - 1) {
+      output += `${key} is ${value}, `
+    } else {
+      output += `${key} is ${value}.`;
+    }
+  }) 
+  console.log(output)
 };
 
 parseArgs();
