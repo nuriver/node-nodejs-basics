cd4eed6967125aa94c3d3264cdcc0cf3dfa39360
Aleksei Golubkov <nuriver@gmail.com>
2024-09-25 14:57:57 +0200

    feat: implement create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile, stat } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fresh.txt");
+
+  stat(filePath, (error) => {
+    if (!error) {
+      throw new Error("FS operation failed");
+    } else {
+      writeFile(filePath, "I am fresh and young", (error) => {
+        if (error) throw error;
+      });
+    }
+  });
 };
 
 await create();
