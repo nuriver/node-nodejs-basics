7dba84046ae37847a852205dfda6648483432d4c
Aleksei Golubkov <nuriver@gmail.com>
2024-09-26 20:22:45 +0200

    feat: implement calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+import {createHash} from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  
+  const readableStream = fs.createReadStream(filePath);
+  const hash = createHash("sha256");
+
+  readableStream.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
