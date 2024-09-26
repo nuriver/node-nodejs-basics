124ad2d1997b3c765bef6b68d3041a9b3d7f1ea8
Aleksei Golubkov <nuriver@gmail.com>
2024-09-26 17:02:36 +0200

    feat: implement read function in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, "files", "fileToRead.txt");
+
+  const readableStream = fs.createReadStream(fileToReadPath, "utf-8");
+  readableStream.on("data", (data) => {
+  stdout.write(data)
+  })
 };
 
 await read();
